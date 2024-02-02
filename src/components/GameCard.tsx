d81e6c540f01416d131318e8f5673e71dc721f4e
Maxi-echaide <maxi.echaide@gmail.com>
2024-02-02 16:09:49 -0300

    intial start, layout, filters: platform, genre.

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,28 @@
+import getCroppedImageUrl from '../services/image-url';
+import CriticScore from './CriticScore';
+import PlatformIconList from './PlatformIconList';
+import { Game } from './hooks/useGames';
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card >
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <Heading fontSize='2xl'>{game.name}</Heading>
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
