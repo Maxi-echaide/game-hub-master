0fe0348cf0d4f94a8dbc746ef6748b0d50432462
Maxi-echaide <maxi.echaide@gmail.com>
2024-02-02 16:52:56 -0300

    refactor extract query object

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-
-import './App.css'
+import './App.css';
 import { Box, Flex, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
 import { useState } from 'react';
@@ -9,11 +8,13 @@ import { Genre } from './components/hooks/useGenres';
 import PlatformSelector from './components/PlatformSelector';
 import { Platform } from './components/hooks/useGames';
 
-
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
   return (
     <Grid
@@ -32,18 +33,23 @@ function App() {
       <Show above='lg'>
         <GridItem area='aside' paddingX={5}>
           <GenreList
-            selectedGenre={selectedGenre}
-            onSelectedGenre={(genre) => setSelectedGenre(genre)}
+            selectedGenre={gameQuery.genre}
+            onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
           />
         </GridItem>
       </Show>
 
       <GridItem area='main'>
-        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
-        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
+        <PlatformSelector
+          selectedPlatform={gameQuery.platform}
+          onSelectPlatform={(platform) =>
+            setGameQuery({ ...gameQuery, platform })
+          }
+        />
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
   );
 }
 
-export default App
+export default App;
